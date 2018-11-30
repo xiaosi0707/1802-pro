4fb9704c1335f1656714e245e3767d3e76a57eed
wyunfei <583514368@qq.com>
2018-11-30 17:51:55 +0800

    组件封装的开放性、封闭性、粒度

--- components/like/index.js
+++ components/like/index.js
@@ -1,11 +1,14 @@
 // components/like/index.js
 Component({
   data: {
-    like: true, // 数据绑定
-    count1: 99,
-    count2: 999,
-    yesSrc: 'images/like.png',
-    noSrc: 'images/like@dis.png'
+    /**
+     * 封闭性、开放性、粒度
+     * 
+    */
+    like: true, // 数据绑定，应该开放的数据由外部进行控制
+    count: 9, // 开放
+    yesSrc: 'images/like.png', // 只有组件内部用到，因此无需开放
+    noSrc: 'images/like@dis.png' // 无需开放
   }, 
   /**
  * 组件的方法列表
