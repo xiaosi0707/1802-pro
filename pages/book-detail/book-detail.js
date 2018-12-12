e9c55c92c92535029e900f2897c3e6ebba8017df
wyunfei <583514368@qq.com>
2018-12-12 08:57:53 +0800

    书籍详情点赞

--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -1,6 +1,8 @@
 // pages/book-detail/book-detail.js
 import { BookModel } from '../../models/book.js'
 let bookModel = new BookModel()
+import { LikeModel } from '../../models/like.js'
+let likeModel = new LikeModel()
 Page({
 
   /**
@@ -38,53 +40,8 @@ Page({
       })
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onLike (ev) {
+    const likeOrCancel = ev.detail.behavior
+    likeModel.like(likeOrCancel, this.data.detail.id, 400)
   }
 })
