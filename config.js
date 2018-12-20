c990c0f07fad37ffbc1ed5ede5f54e30faf59f00
xiaosi0707 <583514368@qq.com>
2018-12-20 15:21:43 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const config = {
   api_base_url: 'http://bl.7yue.pro/v1/',
   wx_base_url: 'wechatAPI',
-  appKey: '5ZbxAY1FmDNQP1T1'
+  appKey: '5ZbxA7Y1F7mDN7QP17T1'
 }
 // 第二种写法，一起导出
 export { config }
