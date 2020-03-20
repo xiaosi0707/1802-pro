5923249887df82d9b362c0c5d545de08ce3afb41
xiaosi0707 <583514368@qq.com>
2020-03-20 16:14:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // 全局配置
 const config = {
-  api_base_url: 'http://bl.7yue.pro/v1/',
+  api_base_url: 'http://www.usian.net/v1/',
   wx_base_url: 'wechatAPI',
-  appKey: '5ZbxA7Y1F7mDN7QP17T1'
+  appKey: ''
 }
 // 第二种写法，一起导出
 export { config }
